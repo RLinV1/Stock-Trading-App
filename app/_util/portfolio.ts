a3af1820ea359b9e63c73c8d241ecef11ba66985
Raymond Lin <86033049+RLinV1@users.noreply.github.com>
2026-03-20 19:17:59 -0400

    Update portfolio.ts

--- app/_util/portfolio.ts
+++ app/_util/portfolio.ts
@@ -2,7 +2,7 @@ import axios from "axios"
 import { PortfolioSnapshot } from "../_types/types"
 
 export const getPortfolioSnapshots = async (userId: string):  Promise<PortfolioSnapshot[]> => {
-    const res = await axios.get("https://stock-trading-app-backend-production.up.railway.app//api/portfolio/" + userId , 
+    const res = await axios.get("https://stock-trading-app-backend-production.up.railway.app/api/portfolio/" + userId , 
         {withCredentials: true}
     )
 
